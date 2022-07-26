c6c3bf2a7430dff3153c81f248537eb29b12b9b4
Anil <anil.antony@settlin.in>
2022-07-26 23:39:30 +0530

    fix: model created

---
+++ Server/server/model/eventmodel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const movieDb = require('./moviemodel');
+
+var eventSchema = {
+    eventName : {
+        type: String,
+        required : true,
+        unique : true
+    },
+    hostedBy : String,
+    movies : [movieDb],
+    sumbmissions : Number,
+    dueDate : Date,
+    startDate : Date,
+    maxEntries : Number,
+    open : Boolean,
+}
+
+var eventDb = mongoose.model('events',eventSchema);
+module.exports = eventDb;
