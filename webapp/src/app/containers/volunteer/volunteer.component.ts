d84c7a6d3511bae79933fb7396701b53bc406d25
Anil <anil.antony@settlin.in>
2022-08-08 21:14:25 +0530

    fix: added table for volunteer

--- webapp/src/app/containers/volunteer/volunteer.component.ts
+++ webapp/src/app/containers/volunteer/volunteer.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { VolunteerServicesService } from './services/volunteer-services.service';
 import {KeyValue} from '@angular/common';
+import $ from "jquery";
 
 @Component({
   selector: 'app-volunteer',
@@ -11,13 +12,15 @@ import {KeyValue} from '@angular/common';
 
 export class VolunteerComponent implements OnInit {
 
+  image: any;
+
   singleevent = {
     eventName: '',
     maxEntries: '',
     startDate: '',
     dueDate: '',
     desc: '',
-    hostedBy: ''
+    hostedBy: '',
 }
   Guest = {
     name: '',
@@ -34,8 +37,9 @@ export class VolunteerComponent implements OnInit {
   ngOnInit(): void {
     this.fetchEventData();
   }
-
-
+  file1:any;
+  file2:any;
+  imagesData:any;
   Emodal = false;
   Gmodal = false;
 
@@ -51,6 +55,7 @@ export class VolunteerComponent implements OnInit {
     this.Emodal = false;
   }
   fetchEventData(): any{
+    this.imagesData = null
     return this.event.fetchEvent()
     .subscribe(
       res => {
@@ -66,4 +71,40 @@ export class VolunteerComponent implements OnInit {
   flibrary(){
     
   }
+
+  fileUpload(event: Event){
+    console.log('This is getting called',event.target)
+    const file:any = event.target;
+    this.image.patchValue({image: file})
+    const allowedMimeTypes = ["image/png","image/jpg","image/jpeg"];
+    if (file && allowedMimeTypes.includes(file.type)){
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imagesData= reader.result as string;
+      }
+      reader.readAsDataURL(file);
+    }
+  }
+
+  // fileUpload(event:any){
+  //   this.file1 = event.target.value;
+  //   this.file2 = event.target.files[0];
+  // this.imagesPreview = (event:any,placeToInsertImagePreview:any) => {
+  //   console.log(this.file1) ;
+  //   console.table(this.file2);
+  //   if(this.file1){
+  //     let reader = new FileReader();
+  //     reader.onload = ((event) => {
+  //       $($.parseHTML("<img>"))
+  //         .attr("src", this.file1)
+  //         .appendTo(placeToInsertImagePreview);
+  //     });
+  //     reader.readAsDataURL(this.file1)
+  //   }
+  //  }
+  //  $("#input-files").on("change",() => {
+  //     this.imagesPreview(this,"div.preview-images")
+  //  })
+  // }
 }
+
