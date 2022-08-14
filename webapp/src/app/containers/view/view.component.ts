864c23cd509238a3c7915a0db886bfe02cd4b12e
sree125 <sreehariahari125@gmail.com>
2022-08-14 14:17:59 +0530

    view

--- webapp/src/app/containers/view/view.component.ts
+++ webapp/src/app/containers/view/view.component.ts
@@ -1,26 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewservisesService } from './servises/viewservises.service';
-
 @Component({
   selector: 'app-view',
   templateUrl: './view.component.html',
   styleUrls: ['./view.component.css']
 })
 export class ViewComponent implements OnInit {
-  vMoviedata:any;
-  constructor(
-    public getmovie:ViewservisesService) {
-      this.fetchvmovie();
-     }
+  GMoviedata:any;
+  constructor(public getmovie:ViewservisesService) { 
+    this.fetchgmovie();
+  }
 
   ngOnInit(): void {
   }
-  fetchvmovie(){
-    return this.getmovie.getvmovie().subscribe((movies) => {
+  fetchgmovie(){
+    return this.getmovie.getGmovie().subscribe((movies) => {
       console.log(movies);
-      this.vMoviedata = Object.values(movies);
-      console.log(this.vMoviedata);
+      this.GMoviedata = Object.values(movies);
+      console.log(this.GMoviedata);
     })
   }
-
 }
