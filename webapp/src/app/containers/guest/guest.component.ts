a8d6e22c6bfe68ce0942ddef5bbfdbe2707299d4
bonjourn <shameem_18a018cs@gecwyd.ac.in>
2022-08-17 00:15:44 +0530

    before cleanup

--- webapp/src/app/containers/guest/guest.component.ts
+++ webapp/src/app/containers/guest/guest.component.ts
@@ -26,17 +26,14 @@ export class GuestComponent implements OnInit {
   MarKs : []
   roleid:string =localStorage.getItem('id') || '';
   exmark:any
-  
-  
   GMoviedata:any;
   GMoviedatam:any;
   GMoviedatan:any;
-  GMoviedatac:any;
-
+  Reviewed:any;
   moviedata1:any;
   flagidtest:boolean=false;
-  GMoviedatab:any;
-
+  validMovies:any;
+  unReviewed: any;
 
   constructor(
     public serve:GuestServiceService,
@@ -52,76 +49,69 @@ export class GuestComponent implements OnInit {
   sguestmodal(movieId:string){
     this.Gmodal = !this.Gmodal;
     this.movieid = movieId;
-    console.log(this.movieid);
   }
   cguestmodal(rvform:NgForm){
     this.Gmodal = !this.Gmodal;
     rvform.reset();
+    window.location.reload();
   }
   sguestmodalmarks(movieId:string,moviename:string,marKs:[]){
     this.Gmodalm = !this.Gmodalm;
     this.movieid = movieId;
-    console.log(this.movieid);
     this.movieName = moviename;
-    console.log(this.movieName);
     this.MarKs = marKs;
-    console.log(this.MarKs);
 
     // to fetch one movie data
     return this.getmovie.getg1movie(this.movieid).subscribe((movies1:any) => {
-      console.log(movies1);
       this.moviedata1=movies1;
       this.moviedata1.marks1=movies1.marks;
-      console.log(this.moviedata1.marks1,"yeahbouuuuuy");
       this.roleid=localStorage.getItem('id') || '';
-      
       for(var i in this.moviedata1.marks1){
-        console.log(this.moviedata1.marks1[i],i);
         if(this.moviedata1.marks1[i]._id == this.roleid){
-          console.log(this.moviedata1.marks1[i],"if works");
           this.exmark=this.moviedata1.marks1[i];
-          console.log(this.exmark,"hoooraaaaay");
         }
       }
     })
-
   }
   cguestmodalmarks(){
     this.Gmodalm = !this.Gmodalm;
   }
   addReview(rvform:NgForm):void{
     this.marks._id = localStorage.getItem('id') || '';
-    console.log(this.marks);
     this.serve.updatemark(this.marks,this.movieid);
     this.Gmodal = false;
     rvform.reset();
+    window.location.reload();
   }
   fetchgmovie(){
     return this.getmovie.getGmovie().subscribe((movies) => {
-      this.GMoviedatac=[];
-      this.GMoviedatab=[];
-      console.log(movies);
+      this.Reviewed=[];
+      this.unReviewed=[];
+      this.validMovies=[];
       this.GMoviedata = Object.values(movies);
       this.GMoviedatam = movies;
       this.GMoviedatan = movies;
 
       // to bereviewed feature
-      // this.GMoviedatan.map((movieb: { guests: any[]; })=>{
-      //   movieb.guests.forEach((e: string)=>{
-      //     if(e==this.roleid) this.GMoviedatab.push(movieb)
-      //   })
-      // })
-      // console.log(this.GMoviedatab);
-
-      // reviewed feature
-      this.GMoviedatam.map((movie: { marks: any[]; })=>{
-        movie.marks.forEach((e: { _id: string; })=>{
-          if(e._id==this.roleid) this.GMoviedatac.push(movie)
+      this.GMoviedatan.forEach((movieb:
+        {guests: any[];})=>{
+        movieb.guests?.forEach((f: string)=>{
+          if(f==this.roleid) {
+            this.validMovies.push(movieb)
+          }
         })
       })
 
-      console.log(this.GMoviedatac);
+      // reviewed feature
+      this.validMovies.map((movie: { marks: any[]; })=>{
+        if(!movie.marks.length){
+          this.unReviewed?.push(movie)
+        }
+        movie.marks.forEach((e: { _id: string; })=>{
+          if(e._id==this.roleid){ 
+            this.Reviewed?.push(movie)}
+        })
+      })
     })
   }
-
 }
