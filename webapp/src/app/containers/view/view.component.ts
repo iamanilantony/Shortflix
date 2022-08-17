010dd26e6a34769cab29e6ab82a91e516b9dc6d8
bonjourn <shameem_18a018cs@gecwyd.ac.in>
2022-08-17 20:09:19 +0530

    genere feature added

--- webapp/src/app/containers/view/view.component.ts
+++ webapp/src/app/containers/view/view.component.ts
@@ -7,6 +7,13 @@ import { ViewservisesService } from './servises/viewservises.service';
 })
 export class ViewComponent implements OnInit {
   GMoviedata:any;
+  GMoviedatan: any;
+  actionMovies: any;
+  comedyMovies: any;
+  docuMovies: any;
+  adventureMovies: any;
+  musicalMovies: any;
+  scifiMovies: any;
   constructor(public getmovie:ViewservisesService) { 
     this.fetchgmovie();
   }
@@ -17,7 +24,37 @@ export class ViewComponent implements OnInit {
     return this.getmovie.getGmovie().subscribe((movies:any) => {
       console.log(movies);
       this.GMoviedata = Object.values(movies);
-      console.log(this.GMoviedata);
+      this.GMoviedatan = movies;
+      console.log(this.GMoviedatan);
+
+      this.actionMovies=[]
+      this.comedyMovies=[]
+      this.docuMovies=[]
+      this.adventureMovies=[]
+      this.musicalMovies=[]
+      this.scifiMovies=[]
+
+      this.GMoviedatan.forEach((movieb: { genre: string; })=>{
+          console.log(movieb,"movieb")
+          if(movieb.genre=="Action") {
+            this.actionMovies.push(movieb)
+          }
+          if(movieb.genre=="Comedy") {
+            this.comedyMovies.push(movieb)
+          }
+          if(movieb.genre=="Documentry") {
+            this.docuMovies.push(movieb)
+          }
+          if(movieb.genre=="Adventure") {
+            this.adventureMovies.push(movieb)
+          }
+          if(movieb.genre=="Musical") {
+            this.musicalMovies.push(movieb)
+          }
+          if(movieb.genre=="Sci-Fi") {
+            this.scifiMovies.push(movieb)
+          }
+      })
     })
   }
 }
